895a5c006ac8b3021be25d39b1a6fbe47ebc2b63
Jessie UGOLIN <jessie.ugolin@gmail.com>
2024-10-18 12:50:05 +0200

    search IP form

---
+++ README.md
@@ -0,0 +1,14 @@
+## Stack
+- Symfony 7
+- Tailwind
+- StimulusJs
+## Lib
+- symfony/ux-map (leaflet)
+- symfony/ux-icon
+- jenssegers/agent
+- symfonycasts/tailwind-bundle
+- [tailwindcss-stimulus-components](https://github.com/excid3/tailwindcss-stimulus-components)
+
+## RUN on local
+- `docker-compose up --build -d`
+- see `localhost:82`
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\SearchIpType;
 use Jenssegers\Agent\Agent;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\UX\Map\Map;
-use Symfony\UX\Map\Marker;
 use Symfony\UX\Map\Point;
 
 class DefaultController extends AbstractController
@@ -18,44 +18,56 @@ class DefaultController extends AbstractController
     public function __construct(private HttpClientInterface $client) {}
 
     #[Template('default/index.html.twig')]
-    #[Route('/', name: 'app_default')]
-    public function index(Request $request): array
+    #[Route('/{ip}', name: 'app_default', requirements: ['ip' => '\d{1,3}(\.\d{1,3}){3}'], defaults: ['ip' => null])]
+    public function index(?string $ip, Request $request): array|Response
     {
         $ipInfo = [];
-
         $map = null;
         $apiUrl = $this->getParameter(name: 'api.ip_url');
 
-        $ipInfo['IP'] = '91.160.145.143'; //$request->getClientIp();
+        $ipInfo['IP'] = $ip ?: $request->getClientIp(); // Real IP
+
+        $search_form = $this->createForm(SearchIpType::class, ['IP' => $ipInfo['IP']]);
+        $search_form->handleRequest($request);
+        if ($search_form->isSubmitted() && $search_form->isValid()) {
+            $data = $search_form->getData();
+            return $this->redirectToRoute('app_default', ['ip' => $data['IP']]);
+        }
+
         $ipInfo['Port'] = $request->getPort();
 
-        $response = $this->client->request('GET', $apiUrl . $ipInfo['IP']);
-        $data = json_decode($response->getContent(), true);
+        if (filter_var($ipInfo['IP'], FILTER_VALIDATE_IP)) {
 
-        $ping = exec("ping -c 1 google.com", $output, $status);
-        // Vérifier si la requête a réussi
-        if (isset($data['status']) && $data['status'] === 'success') {
-            // Extraire les informations
-            $ipInfo = [
-                'IP' => $data['query'],
-                'City' => $data['city'],
-                'ISP' => $data['isp'],
-                'Country' => $data['country'],
-                'Region' => $data['regionName'],
-                'Timezone' => $data['timezone'],
-                'Latitude' => $data['lat'],
-                'Longitude' => $data['lon'],
-                'Proxy' => isset($data['proxy']) && $data['proxy'] ? 'Yes' : 'No',
-            ];
-            $map = (new Map())
-                ->center(new Point($ipInfo['Latitude'], $ipInfo['Longitude']))
-                ->zoom(12)
-                ->fitBoundsToMarkers();
+            $response = $this->client->request('GET', $apiUrl . $ipInfo['IP']);
+            $data = json_decode($response->getContent(), true);
+
+            $ping = exec("ping -c 1 google.com", $output, $status);
+            // Vérifier si la requête a réussi
+            if (isset($data['status']) && $data['status'] === 'success') {
+                // Extraire les informations
+                $ipInfo = [
+                    'IP' => $data['query'],
+                    'City' => $data['city'],
+                    'ISP' => $data['isp'],
+                    'Country' => $data['country'],
+                    'Region' => $data['regionName'],
+                    'Timezone' => $data['timezone'],
+                    'Latitude' => $data['lat'],
+                    'Longitude' => $data['lon'],
+                    'Proxy' => isset($data['proxy']) && $data['proxy'] ? 'Yes' : 'No',
+                ];
+                $map = (new Map())
+                    ->center(new Point($ipInfo['Latitude'], $ipInfo['Longitude']))
+                    ->zoom(12)
+                    ->fitBoundsToMarkers();
+            } else {
+                // Gérer les cas où l'API renvoie une réponse invalide
+                $ipInfo['error'] = 'Invalid IP information received from API';
+            }
         } else {
-            // Gérer les cas où l'API renvoie une réponse invalide
-            $ipInfo['error'] = 'Invalid IP information received from API';
+            $ipInfo['error'] = 'Invalid IP address';
         }
-        
+
         $agent = new Agent();
         $other_info['device'] = $agent->isMobile() ? 'Mobile' : ($agent->isTablet() ? 'Tablet' : 'Desktop');
         $other_info['browser'] = $agent->browser();
@@ -65,7 +77,8 @@ class DefaultController extends AbstractController
             'ip_info' => $ipInfo,
             'map' => $map,
             'other_info' => $other_info,
-            'ping_status' => $status
+            'ping_status' => $status,
+            'form' => $search_form
         ];
     }
 }
